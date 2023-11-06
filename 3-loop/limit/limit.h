6519b4b25e0813f265e2bfe9c3be3439d2c1183c
184amos8a@mail.ru <145807981+karinaamos@users.noreply.github.com>
2023-11-06 19:42:10 +0300

    123

--- 3-loop/limit/limit.h
+++ 3-loop/limit/limit.h
@@ -1,5 +1,25 @@
 #include <math.h>
 
 double limit(double eps, int n_max){
-    return 0.0;
-}
+    double x_n, x_1, x_2 = 0, n_1 = 1;
+    if (n_max>1){
+
+        x_1 = n_1 / sqrt((pow(n_1, 2) + n_1));
+
+        while(fabs(x_1 - x_2) > eps){
+            if (n_1 <= n_max){
+            
+            n_1+=1;
+            x_2 = x_1;
+            x_1 = n_1 / sqrt((pow(n_1, 2) + n_1));
+
+            }
+            else
+                x_1 = x_2;
+        }
+        return x_1;
+    }
+    else
+        x_n = n_max / sqrt((pow(n_max, 2) + n_max));
+        return x_n;
+} 
