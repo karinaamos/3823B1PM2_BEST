80026bcd4a9b281fa555782800114dc60cd773f0
184amos8a@mail.ru <145807981+karinaamos@users.noreply.github.com>
2023-11-06 19:41:21 +0300

    123

--- 3-loop/find_elem/find_elem.h
+++ 3-loop/find_elem/find_elem.h
@@ -1,8 +1,22 @@
 #include <math.h>
 
 int find_elem(int left, int right, int number, int max_iter){
-    int i = 0; //Счётчик итераций
-
-
+    int i = 0;
+    int flag = 1;
+    int c = 0, le = left, ri = right, poisk = number;
+    while (flag > 0){
+        c = (le + ri) / 2;
+        i += 1;
+        if (c == poisk)
+            return i * (poisk - left);
+        else if (c < poisk)
+            le = c + 1;
+        else if (c > poisk)
+            ri = c - 1;
+        if (i > poisk){
+            i = 0;
+            flag = 0;
+            }
+    }
     return i; //НЕ МЕНЯТЬ!
 }
