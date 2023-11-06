c60302c940c64effe52e2517f132c3d9d0ab40cd
184amos8a@mail.ru <145807981+karinaamos@users.noreply.github.com>
2023-11-06 19:41:45 +0300

    123

--- 3-loop/integral/integral.h
+++ 3-loop/integral/integral.h
@@ -1,5 +1,11 @@
 #include <math.h>
 
 double integral(double a, double b, double c, double left, double right){
-    return 0.0;
+    double yrav2 = (a * right * right * right / 3) + (b * right * right / 2) + c * right;
+    double yrav1 = (a * left * left * left / 3) + (b * left * left / 2) + c * left;
+    double otv = yrav2 - yrav1;
+    if (otv < 0)
+        return -1;
+    else
+        return otv;
 }
