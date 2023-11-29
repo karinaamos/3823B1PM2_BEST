d35bdfab06524494c4ee73f049f21b8ab8bed0f4
karinaamos <184amos8a@mail.ru>
2023-11-29 13:50:22 +0300

    aa

--- 6-functions/max_min/max_min_finder.h
+++ 6-functions/max_min/max_min_finder.h
@@ -1,5 +1,15 @@
 #include <math.h>
 
 void get_max_min(int* array, int size, int* pMax, int* pMin){
+    int maxim = -10000;
+    int minim =  10000;
+    for (int i = 0; i < size; i ++){
+        if (array[i] > maxim)
+            maxim = array[i];
+        if (array[i] < minim)
+            minim = array[i];
+    }
+    *pMax = maxim;
+    *pMin = minim;
     return;
 }
