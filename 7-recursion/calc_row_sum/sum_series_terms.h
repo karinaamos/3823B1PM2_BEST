58316399fb85ada5a22638cc4a1e14fe8f1e332d
karinaamos <184amos8a@mail.ru>
2023-12-06 18:55:01 +0300

    12

--- 7-recursion/calc_row_sum/sum_series_terms.h
+++ 7-recursion/calc_row_sum/sum_series_terms.h
@@ -1,5 +1,33 @@
 #include <math.h>
 
+double f(int n, int a){
+    if (a == 1){
+        return 1.0;
+        }
+    else if (a == n+1){
+        return 1.0 / a;
+    }
+    else {
+        return 1.0 / a + f(n, a - 1);
+    }
+}
+
 double sum_series(int n){
-    return -1.0;
+    double k = 0.0;
+    if (n <= 0)
+        return -1;
+    if (n > 10000){
+        int x1 = n /10000;
+        for (int i = 1; i < x1; i++){
+            x1 += f((i-1) * 10000, i * 10000);
+        }
+        int x2 = n % 10000;
+        for (int i = 0; i < x2; i++){
+            k += f((i-1) + 10000 * x1 , i + 10000 * x1);
+        }
+    }
+    else {
+        k += f(0, n);
+    }
+    return k; 
 }
