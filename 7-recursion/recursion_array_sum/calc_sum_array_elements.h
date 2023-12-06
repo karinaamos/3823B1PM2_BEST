c489bc348297925a3f7454e8a4e14845a27f23fb
karinaamos <184amos8a@mail.ru>
2023-12-06 20:11:03 +0300

    06.12

--- 7-recursion/recursion_array_sum/calc_sum_array_elements.h
+++ 7-recursion/recursion_array_sum/calc_sum_array_elements.h
@@ -1,5 +1,17 @@
 #include <math.h>
 
-double calc_sum_elements(int* array, int array_size){
-    return -1.0;
+int f(int* array, int array_size)
+{
+    if (array_size == 0)
+        return array[0];
+    else
+        return array[array_size] + f(array, array_size - 1);
+}
+
+int calc_sum_elements(int* array, int array_size){
+    if (array_size <= 0)
+        return -1;
+    else 
+        return f(array, array_size - 1);
+    
 }
--- 7-recursion/revers_array/revers.h
+++ 7-recursion/revers_array/revers.h
@@ -1,5 +1,18 @@
 #include <math.h>
 
-void reverse_array(int* array, int array_size){
+void reverse(int* array, int size, int count){
+    if (size == 0){
+        return;
+    }
+    if (++count >= --size){
+        return;
+    }
+    int t = array[size];
+    array[size] = array[count];
+    array[count] = t;
+    reverse(array, size, count);
+}
 
+void reverse_array(int* array, int array_size){
+    reverse(array, array_size, -1);
 }
