84f9d85790e9765d65492b8c884f16149800b586
karinaamos <184amos8a@mail.ru>
2023-12-06 19:53:53 +0300

    NOD

--- 7-recursion/NOD/calc_nod.h
+++ 7-recursion/NOD/calc_nod.h
@@ -1,5 +1,17 @@
 #include <math.h>
 
 int nod(int first, int second){
-    return -1;
+    double r;
+    r = first % second;
+    int nod1;
+    if ((first==0)||(second==0)){
+        return -1;
+    }
+    if (r == 0){
+        return second;
+    }
+    else{
+        nod1 = nod(second, r);
+        return nod1;
+    }
 }
