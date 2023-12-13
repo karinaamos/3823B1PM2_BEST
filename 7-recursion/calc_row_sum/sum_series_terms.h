c7c882c9b36ad870fc850fdcea15ea4ef8bf2033
karinaamos <184amos8a@mail.ru>
2023-12-13 19:58:49 +0300

    13.12

--- 7-recursion/calc_row_sum/sum_series_terms.h
+++ 7-recursion/calc_row_sum/sum_series_terms.h
@@ -1,4 +1,13 @@
 #include <math.h>
 
 double sum_series(int n){
+    if (n < 1){
+        return -1;
+    }
+    if (n == 1){
+        return 1;
+    }
+    else{
+        return 1.0/n + sum_series(n-1);
+    }
 }
--- 7-recursion/new_series/find_series_sum.h
+++ 7-recursion/new_series/find_series_sum.h
@@ -1,5 +1,19 @@
 #include <math.h>
 
+
+double _find_sum_elements_series(int k){
+    return k == 0 ? 0 :(_find_sum_elements_series(k - 1) + 1) / k;
+}
+
 double find_sum_elements_series(int k){
-    return -1.0;
+    if (k < 0)
+        return - 1;
+
+    double sum = 0;
+
+    for (int i = 0; i < k; i++){
+        sum += _find_sum_elements_series(i);
+    }
+
+    return sum;
 }
